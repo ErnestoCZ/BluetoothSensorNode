82dd07eadec5aa6e0f83a999d35603c035b8b797
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-03-24 18:34:24 +0100

    added initial and not implemented bme280_mod.c/.h

---
+++ src/lib/bme280_mod.c
@@ -0,0 +1,20 @@
+#include "bme280_mod.h"
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+
+
+int bme280_mod_init(void){
+    //TODO implementation
+    return 0;
+};
+int bme280_mod_start(void){
+    //TODO implementation
+    return 0;
+};
+int bme280_mod_stop(void){
+    //TODO implementation
+    return 0;
+};
---
+++ src/lib/bme280_mod.h
@@ -0,0 +1,5 @@
+#pragma once 
+
+int bme280_mod_init(void);
+int bme280_mod_start(void);
+int bme280_mod_stop(void);
