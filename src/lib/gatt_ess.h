883a60199364ba8ff43c48e8d86efe635240c2c1
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-03-28 09:51:05 +0100

    removed unnecassary code from header

--- src/lib/gatt_ess.h
+++ src/lib/gatt_ess.h
@@ -3,19 +3,3 @@
 #include <zephyr/bluetooth/assigned_numbers.h>
 
 void init_gatt_ess(void);
-
-ssize_t bt_temp_read_callback(struct bt_conn *conn,
-					    const struct bt_gatt_attr *attr,
-					    void *buf, uint16_t len,
-					    uint16_t offset);
-
-
-ssize_t bt_humidity_read_callback(struct bt_conn *conn,
-					    const struct bt_gatt_attr *attr,
-					    void *buf, uint16_t len,
-					    uint16_t offset);
-
-ssize_t bt_pressure_read_callback(struct bt_conn *conn,
-					    const struct bt_gatt_attr *attr,
-					    void *buf, uint16_t len,
-					    uint16_t offset);
