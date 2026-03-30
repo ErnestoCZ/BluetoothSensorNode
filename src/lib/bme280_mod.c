e3e7ddafe15f00eb1cb04c6aa27840c6fb5e5ded
Ernest Czechowski <ernest.czechowski@gmail.com>
2026-03-30 06:43:08 +0200

    renamed variables, added semaphore support to indicate data is ready, added compare function for bme280 value struct

--- src/lib/bme280_mod.c
+++ src/lib/bme280_mod.c
@@ -7,18 +7,19 @@
 
 LOG_MODULE_REGISTER(BME280_MOD);
 
-#define PRIORITY 7
-#define STACK_SIZE 1024
-#define WORK_DELAY K_SECONDS(30)
-#define MUTEX_TIMEOUT K_FOREVER
+#define BME280_WORK_QUEUE_PRIORITY 7
+#define BME280_STACK_SIZE 1024
+#define BME280_WORK_DELAY K_SECONDS(30)
+#define BME280_MUTEX_TIMEOUT K_FOREVER
 #define BME280_NODE DT_ALIAS(env_sensor) 
 const struct device* bme280_dev = DEVICE_DT_GET_OR_NULL(BME280_NODE);
 static struct k_work_q bme_work_queue;
 static struct k_work_delayable bme_work_del;
-static struct bme280_values bme280_data = {};
+static struct bme280_values_t bme280_data = {0.0};
 
-K_MUTEX_DEFINE(bme_mutex);
-K_THREAD_STACK_DEFINE(stack, STACK_SIZE);
+K_SEM_DEFINE(bme280_data_ready_sem,0,1);
+K_MUTEX_DEFINE(bme280_mutex);
+K_THREAD_STACK_DEFINE(bme280_work_queue_stack, BME280_STACK_SIZE);
 void bme280_work_handler(struct k_work* work){
     static struct sensor_value temp, pres, hum;
     int rc = sensor_sample_fetch(bme280_dev);
@@ -30,14 +31,15 @@ void bme280_work_handler(struct k_work* work){
     sensor_channel_get(bme280_dev, SENSOR_CHAN_PRESS, &pres);
     sensor_channel_get(bme280_dev, SENSOR_CHAN_HUMIDITY, &hum);
     
-    k_mutex_lock(&bme_mutex, MUTEX_TIMEOUT);
+    k_mutex_lock(&bme280_mutex, BME280_MUTEX_TIMEOUT);
     bme280_data.temperature = sensor_value_to_double(&temp);
     bme280_data.pressure = sensor_value_to_double(&pres);
     bme280_data.humidity = sensor_value_to_double(&hum);
-    k_mutex_unlock(&bme_mutex);
+    k_mutex_unlock(&bme280_mutex);
+    k_sem_give(&bme280_data_ready_sem);
     // LOG_DBG("New BME280 values : %d Celsius | %d kPa | %d ",temp.val1,pres.val1,hum.val1);
 reschedule:    
-    k_work_reschedule_for_queue(&bme_work_queue,&bme_work_del, WORK_DELAY);
+    k_work_reschedule_for_queue(&bme_work_queue,&bme_work_del, BME280_WORK_DELAY);
     
 };
 
@@ -51,7 +53,7 @@ int bme280_mod_init(void){
         return -ENODEV;
     }
     k_work_queue_init(&bme_work_queue);
-    k_work_queue_start(&bme_work_queue,stack,K_THREAD_STACK_SIZEOF(stack),PRIORITY,NULL);
+    k_work_queue_start(&bme_work_queue,bme280_work_queue_stack,K_THREAD_STACK_SIZEOF(bme280_work_queue_stack),BME280_WORK_QUEUE_PRIORITY,NULL);
     k_thread_name_set(&bme_work_queue.thread,"bme280_wq");
     k_work_init_delayable(&bme_work_del,bme280_work_handler);
     
@@ -68,12 +70,25 @@ void bme280_mod_stop(void){
     k_work_cancel_delayable(&bme_work_del);
 };
 
-int bme280_get_latest_data(struct bme280_values *dest){
+int bme280_get_latest_data(struct bme280_values_t *dest){
     if(dest == NULL){
         return -EINVAL;
     }
-    k_mutex_lock(&bme_mutex,MUTEX_TIMEOUT);
+    k_mutex_lock(&bme280_mutex,BME280_MUTEX_TIMEOUT);
     *dest = bme280_data;
-    k_mutex_unlock(&bme_mutex);
+    k_mutex_unlock(&bme280_mutex);
+    return 0;
+};
+
+
+int bme280_data_has_changed(const struct bme280_values_t *s1, const struct bme280_values_t *s2){
+    if(s1 == NULL || s2 == NULL) return -EINVAL;
+    
+    if(
+        s1->humidity != s2->humidity ||
+        s1->pressure != s2->pressure ||
+        s1->temperature != s2->temperature
+    ) return 1;
+
     return 0;
 };
--- src/lib/bme280_mod.h
+++ src/lib/bme280_mod.h
@@ -1,12 +1,46 @@
 #pragma once 
+#include <zephyr/kernel.h>
 
-struct bme280_values{
+extern struct k_sem bme280_data_ready_sem;
+
+struct bme280_values_t{
     double temperature;
     double pressure;
     double humidity;
 };
 
+/**
+ * @brief Performs bme280 corresponding initializations
+ * 
+ * @return -ENODEV if bme280 device does not exists, 0 by success
+ */
 int bme280_mod_init(void);
+
+/**
+ * @brief Starts bme280_mod corresponding work
+ * 
+ */
 void bme280_mod_start(void);
+
+/**
+ * @brief Stops bme280_mod corresponding work
+ * 
+ */
 void bme280_mod_stop(void);
-int bme280_get_latest_data(struct bme280_values *dest);
+
+/**
+ * @brief Helper to get the latest values sampled by the bme280 sensor
+ * 
+ * @param dest 
+ * @return int -EINVAL if passed struct is NULL, 0 by success
+ */
+int bme280_get_latest_data(struct bme280_values_t *dest);
+
+/**
+ * @brief Helper to compare bme280_value_t structs
+ * 
+ * @param s1 
+ * @param s2 
+ * @return int -EINVAL if at least one passed struct is NULL, 1 if at least one value in struct changed, 0 if noting changed
+ */
+int bme280_data_has_changed(const struct bme280_values_t *s1, const struct bme280_values_t *s2);
